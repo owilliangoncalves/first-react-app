8f3e261c58b4fc16bb7f8b49fb27d7e7e77a77ea
Willian Goncalves <williangoncalves74@gmail.com>
2024-04-13 22:07:21 -0300

    Firs Commit

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client';
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
